2a7968f3e9f817c5514b1e7ef327c8a37a13a3d2
kaviyaj5 <92967298+kaviyaj5@users.noreply.github.com>
2021-10-22 15:16:00 +0530

    Add files via upload

---
+++ form.py
@@ -0,0 +1,8 @@
+from tkinter import *
+from tkinter import ttk
+window = Tk()
+window.title("welcome")
+window.geometry('250x250')
+window.configure(background = "black")
+ttk.Button(window, text="TO,MY HOME").grid()
+window.mainloop()
